6581267ea1e36fa37ce714a1c1743f177e59ffbf
Dudarenko Dmitry <102149674+dudarenko-dmitry@users.noreply.github.com>
2024-01-14 01:55:18 +0300

    create Entities

---
+++ src/main/java/pl/senla/pricer/entity/Product.java
@@ -0,0 +1,32 @@
+package pl.senla.pricer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 5L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
---
+++ src/main/java/pl/senla/pricer/entity/User.java
@@ -0,0 +1,38 @@
+package pl.senla.pricer.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "user",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"email"})
+)
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+}
